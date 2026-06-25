de6cd906b5df8aab7e748b1a53248ed42416ba38
rainz27 <zengxiayu0027@gmail.com>
2026-06-25 04:26:52 +0000

    Add userspace test files

---
+++ 02_char_device/chardev_ioctl/user_test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "chardev_ioctl.h"
+
+int main(void){
+    int len;
+    int setLen = 5;
+    int fd = open("/dev/dev1", O_RDWR);
+    if (fd < 0){
+        perror("open");
+        return 1;
+    }
+
+    if (ioctl(fd, CHARDEV_IOCTL_CLEAR) < 0){
+        perror("ioctl CLEAR");
+        close(fd);
+        return 1;
+    }
+
+    if (ioctl(fd, CHARDEV_IOCTL_SETLEN, &setLen) < 0){
+        perror("ioctl SETLEN");
+        close(fd);
+        return 1;
+    }
+
+    printf("Set buffer length to %d.\n", setLen);
+
+    if (ioctl(fd, CHARDEV_IOCTL_GETLEN, &len) < 0){
+        perror("ioctl GETLEN");
+        close(fd);
+        return 1;
+    }
+
+    printf("Get buffer length is %d.\n", len);
+
+	close(fd);
+	return 0;
+}
